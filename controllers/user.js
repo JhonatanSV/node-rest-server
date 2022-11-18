d6961fe8309fa2a4f1b9b430bd5a4f8e65791d58
JhonatanSV <jsandoval@vichara.com>
2022-11-18 17:36:36 -0500

    Db connection

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
 const { response, request } = require("express");
+const bcryptjs = require("bcryptjs");
+
+const User = require("../models/user");
 
 const getUsers = (req = request, res = response) => {
   const params = req.query;
@@ -9,12 +12,24 @@ const getUsers = (req = request, res = response) => {
   });
 };
 
-const postUser = (req, res = response) => {
-  const body = req.body;
+const postUser = async (req, res = response) => {
+  const { name, mail, password, role } = req.body;
+  const user = new User({ name, mail, password, role });
+
+  const existsEmail = await User.findOne({ mail });
+  if (existsEmail) {
+    return res.status(400).json({
+      msg: "The mail already exists",
+    });
+  }
+
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
+
+  await user.save();
 
   res.json({
-    msg: "",
-    body,
+    user,
   });
 };
 
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,31 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
+
 const {
   getUsers,
   putUser,
   postUser,
   deleteUser,
 } = require("../controllers/user");
+const { validateFields } = require("../middlewares/validate-fields");
 
 const router = Router();
 
 router.get("/", getUsers);
-router.post("/", postUser);
+router.post(
+  "/",
+  [
+    check("mail", "The mail is not valid").isEmail(),
+    check("name", "The name is obligatory").not().isEmpty(),
+    check(
+      "password",
+      "The password must be greater than 6 characters"
+    ).isLength({ min: 6 }),
+    check("role", "the role is not valid").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    validateFields,
+  ],
+  postUser
+);
 router.put("/:id", putUser);
 router.delete("/:id", deleteUser);
 
