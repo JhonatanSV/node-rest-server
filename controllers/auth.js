9389cbaffd21a8e1889d4ec097c0ea95a2f86b1e
JhonatanSV <jsandoval@vichara.com>
2022-11-22 22:51:30 -0500

    google signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require("bcryptjs");
 
 const User = require("../models/user");
 const { generateJWT } = require("../helpers/JWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { mail, password } = req.body;
@@ -42,6 +43,48 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, picture, mail } = await googleVerify(id_token);
+
+    let user = await User.findOne({ mail });
+    if (!user) {
+      const data = {
+        name,
+        mail,
+        password: ":p",
+        img: picture,
+        role: "USER_ROLE",
+        google: true,
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    if (!user.state) {
+      return res.status(400).json({
+        msg: "User inactive",
+      });
+    }
+
+    const token = await generateJWT(user.id);
+
+    res.json({
+      msg: "Okay, google SignIn",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "The token couldn't be verified",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth");
+const { login, googleSignIn } = require("../controllers/auth");
 const { validateFields } = require("../middlewares/validate-fields");
 const { validateJWT } = require("../middlewares/validate-jwt");
 
@@ -15,6 +15,12 @@ router.post(
     validateFields,
   ],
   login
+);
+
+router.post(
+  "/google",
+  [check("id_token", "the token is neccesary").not().isEmpty(), validateFields],
+  googleSignIn
 );
 
 module.exports = router;
